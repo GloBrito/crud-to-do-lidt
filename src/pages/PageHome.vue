781cb571bb4ddb8b8d5a2f58940e8bcecde6deaa
Glória Brito <gloriafbrito@gmail.com>
2022-09-09 10:52:24 -0300

    adiciona função de criar tarefa

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,7 +1,27 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <TarefaList msg="Welcome to Your Vue.js App" :tasks="listaDeTarefas" />
+    <nav class="orange darken-2">
+      <div class="nav-wrapper"></div>
+    </nav>
+    <img alt="Vue logo" src="../assets/logo.png" width="32" />
+
+    <div v-show="exibir.lista" style="padding 20px"></div>
+    <button @click="mostrarCadastro">Cadastrar</button>
+
+    <!-- Lista -->
+    <div v-show="exibir.lista">
+      <TarefaList
+        msg="Welcome to Your Vue.js App"
+        :tasks="listaDeTarefas"
+      ></TarefaList>
+    </div>
+
+    <!-- FORM -->
+    <div v-show="exibir.form">
+      <h2>Cadastrar tarefa</h2>
+      <input type="text" name="title" v-model="form.title" />
+      <button class="btn" @click="salvarTarefa">SALVAR</button>
+    </div>
   </div>
 </template>
 
@@ -16,13 +36,40 @@ export default {
   },
   data: () => {
     return {
-      listaDeTarefas: ["A", "B", "C"],
+      listaDeTarefas: [],
+      exibir: {
+        lista: true,
+        form: false,
+      },
+      form: {
+        title: "",
+      },
     };
   },
+  methods: {
+    listarTarefas() {
+      TasksApi.getTasks((data) => {
+        this.listaDeTarefas = data;
+      });
+    },
+    mostrarCadastro() {
+      this.exibir.lista = false;
+      this.exibir.form = true;
+    },
+    salvarTarefa() {
+      this.exibir.form = false;
+      this.exibir.lista = true;
+      const novaTarefa = {
+        title: this.form.title,
+        date: new Date().toLocaleDateString("pt"),
+      };
+      TasksApi.createTask(novaTarefa, () => {
+        this.listarTarefas();
+      });
+    },
+  },
   created() {
-    TasksApi.getTasks((data) => {
-      this.listaDeTarefas = data;
-    });
+    this.listarTarefas();
   },
 };
 </script>
